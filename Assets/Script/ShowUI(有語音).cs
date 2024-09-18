17332052304e5db7a1d92a72e9b4f7cb1c00f355
Edward740420 <153151615+Edward740420@users.noreply.github.com>
2024-09-18 22:25:35 +0800

    Add colider&talk (#16)
    * Azure SDK Speech package
    
    * update script
    
    * add  speech_text
    
    * update script
    
    * update scene
    
    * update scene

---
+++ Assets/Script/ShowUI(有語音).cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowUI2 : MonoBehaviour
+{
+    public GameObject Canva;
+    private AzureSpeechSynthesizer speechSynthesizer;
+
+    void Start()
+    {
+        if (Canva == null)
+        {
+            Debug.LogError("Canva object is not assigned.");
+            return;
+        }
+
+        Canva.SetActive(false);
+
+        speechSynthesizer = Canva.GetComponent<AzureSpeechSynthesizer>();
+        if (speechSynthesizer == null)
+        {
+            Debug.LogError("AzureSpeechSynthesizer component is not attached to the Canva object.");
+        }
+        else
+        {
+            speechSynthesizer.enabled = false; // 初始时禁用
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player")) // 确保只有玩家触发
+        {
+            EnableUIWithSpeech();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player")) // 确保只有玩家触发
+        {
+            DisableUIWithSpeech();
+        }
+    }
+
+    private void EnableUIWithSpeech()
+    {
+        Canva.SetActive(true);
+        if (speechSynthesizer != null)
+        {
+            speechSynthesizer.enabled = true; // 启用脚本
+            speechSynthesizer.TriggerSpeech(); // 开始语音播放
+        }
+    }
+
+    private void DisableUIWithSpeech()
+    {
+        Canva.SetActive(false);
+        if (speechSynthesizer != null)
+        {
+            speechSynthesizer.enabled = false; // 禁用脚本
+        }
+    }
+}
